dde0a49d85826cc62a1a0dee1b0ebf75749ca26c
Glenio Carvalho <gleniohenrique.florindo@q2pay.com.br>
2020-11-13 14:25:00 -0300

    style: add trasition delay image

--- src/components/ImageGrid/styles.ts
+++ src/components/ImageGrid/styles.ts
@@ -4,6 +4,7 @@ export const Image = styled.img<{ loaded: boolean }>`
   opacity: ${(props) => (props.loaded ? 1 : 0)};
   width: 100%;
   border-radius: 5px;
-  transition-timing-function: ease;
+  transition-timing-function: ease-in;
   transition-duration: 1s;
+  transition-delay: 0.6s;
 `;
